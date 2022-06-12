7853992356f25d1f0d96b208c8c102998dfe8c87
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2022-06-12 09:34:52 -0300

    Add initial files

---
+++ src/main/java/br/edu/insper/desagil/koalas/DataFrameException.java
@@ -0,0 +1,9 @@
+package br.edu.insper.desagil.koalas;
+
+public class DataFrameException extends RuntimeException {
+	private static final long serialVersionUID = 7871327181143686978L;
+
+	public DataFrameException(String message) {
+		super(message);
+	}
+}
